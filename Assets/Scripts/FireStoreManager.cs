54b2417e7b1d3d0f8cf650f707947b340542e48c
cuong.chu <cuong.chu@amela.vn>
2020-08-19 10:38:22 +0700

    fix package

--- Assets/Scripts/FireStoreManager.cs
+++ Assets/Scripts/FireStoreManager.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase.Extensions;
+using Firebase.Firestore;
 
-public class NewBehaviourScript : MonoBehaviour
+public class FireStoreManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
